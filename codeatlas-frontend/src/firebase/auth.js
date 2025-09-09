58797fd7eb89d6dc3a7d1dcd5ac42000436deb77
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2025-09-08 22:34:39 -0700

    added firebase in .env

--- codeatlas-frontend/src/firebase/auth.js
+++ codeatlas-frontend/src/firebase/auth.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCFJT3yz1OA3MJo3AOSallkfS5SSfV4shI",
-  authDomain: "codeatlas-9d170.firebaseapp.com",
-  projectId: "codeatlas-9d170",
-  storageBucket: "codeatlas-9d170.firebasestorage.app",
-  messagingSenderId: "406743187585",
-  appId: "1:406743187585:web:cd2ea8cd6372ed84790d80",
-  measurementId: "G-8ZGBTH3S8E"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
