0669facaa9f671dd701fc3bdfea4bb6e5fb558c9
Karan Sanghvi <111078325+karansanghvi@users.noreply.github.com>
2025-09-29 18:55:45 -0700

    deletion
    removed websocket code from backend

--- codeatlas-backend/server.js
+++ codeatlas-backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http");
 
 const githubRoutes = require("./routes/githubRoutes");
 const projectsRoutes = require("./routes/projectRoutes");
 const architectureRoutes = require("./routes/architectureRoutes");
-const { initWebSocket, broadcast } = require("./webSocket/webSocket");
 
 const app = express();
 app.use(cors());
@@ -16,17 +14,5 @@ app.use("/api", githubRoutes);
 app.use("/api", projectsRoutes);
 app.use("/api", architectureRoutes);
 
-// Create HTTP server
-const server = http.createServer(app);
-
-// Init WebSocket
-initWebSocket(server);
-
-app.post("/api/push-simulate", (req, res) => {
-  const data = req.body;
-  broadcast(data);
-  res.sendStatus(200);
-});
-
-// Start HTTP + WebSocket server
-server.listen(5000, () => console.log("🚀 Server + WebSocket running on port 5000"));
+// Start HTTP Server
+app.listen(5000, () => console.log("🚀 Server running on port 5000"));
